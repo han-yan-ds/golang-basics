a57ca4df522b5f61e9f656da4f535f56a9007490
han-yan-ds <hanprogramming@gmail.com>
2021-04-14 10:57:53 -0600

    Casting

--- main.go
+++ main.go
@@ -4,29 +4,32 @@ package main
 // syntax for importing multiple packages
 import (
 	"fmt"
-	m "math" // alias m
+	m "math"
 )
 
 // function declaration
 func main() {
 	// variable declarations
-  const stationName string = "Union Square"
-  var nextTrainTime int8
-  var isUptownTrain bool
-  
+	const stationName string = "Union Square"
+	var nextTrainTime int8
+	var isUptownTrain bool
+
 	// variable assignments
-  nextTrainTime = 12
-  isUptownTrain = false
-  
-  fmt.Println("Current station:", stationName)
-  fmt.Println("Next train:", nextTrainTime, "minutes")
-  fmt.Println("Is uptown:", isUptownTrain)
-  
-  nextTrainTime = 3
-  isUptownTrain = true
-  
-  fmt.Println("")
-  fmt.Println("Current station:", stationName)
-  fmt.Println("Next train:", nextTrainTime, "minutes")
-  fmt.Println("Is uptown:", isUptownTrain)
+	nextTrainTime = 12
+	isUptownTrain = false
+
+	// assign as sqrt, CASTING to float64
+	fmt.Println(m.Sqrt(float64(nextTrainTime)))
+
+	fmt.Println("Current station:", stationName)
+	fmt.Println("Next train:", nextTrainTime, "minutes")
+	fmt.Println("Is uptown:", isUptownTrain)
+
+	nextTrainTime = 3
+	isUptownTrain = true
+
+	fmt.Println("")
+	fmt.Println("Current station:", stationName)
+	fmt.Println("Next train:", nextTrainTime, "minutes")
+	fmt.Println("Is uptown:", isUptownTrain)
 }
