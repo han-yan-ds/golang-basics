17c59e2f256fd1d5784879b4b9dcf37c8093bc7e
han-yan-ds <hanprogramming@gmail.com>
2021-04-14 15:07:09 -0600

    Added deck.go

---
+++ deck.go
@@ -0,0 +1,5 @@
+package main
+
+// create custom type (a "class")
+
+type deck []string
--- main.go
+++ main.go
@@ -4,32 +4,14 @@ package main
 // syntax for importing multiple packages
 import (
 	"fmt"
-	m "math"
 )
+
+func newCard() string {
+	return "Five of Diamonds"
+}
 
 // function declaration
 func main() {
-	// variable declarations
-	const stationName string = "Union Square"
-	var nextTrainTime int8
-	var isUptownTrain bool
-
-	// variable assignments
-	nextTrainTime = 12
-	isUptownTrain = false
-
-	// assign as sqrt, CASTING to float64
-	fmt.Println(m.Sqrt(float64(nextTrainTime)))
-
-	fmt.Println("Current station:", stationName)
-	fmt.Println("Next train:", nextTrainTime, "minutes")
-	fmt.Println("Is uptown:", isUptownTrain)
-
-	nextTrainTime = 3
-	isUptownTrain = true
-
-	fmt.Println("")
-	fmt.Println("Current station:", stationName)
-	fmt.Println("Next train:", nextTrainTime, "minutes")
-	fmt.Println("Is uptown:", isUptownTrain)
+	myDeck := deck{"Ace of Spaces", newCard()}
+	fmt.Println(myDeck)
 }
