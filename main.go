567b90c9db1c683db1fe8e62e16b15f61fde74b9
han-yan-ds <hanprogramming@gmail.com>
2021-07-31 13:46:39 -0600

    My own implementation of using channels, a counter, and a while loop to keep main goroutine running till done

--- main.go
+++ main.go
@@ -14,18 +14,30 @@ func main() {
 		"https://amazon.com",
 	}
 
-	ch := make(chan string)
+	numDone := 0
+
+	ch := make(chan int)
 
 	for _, link := range links {
 		go checkLink(link, ch)
 	}
+
+	for { // keep running until all channels have received a value
+		numDone = numDone + <-ch
+		if numDone == len(links) {
+			return
+		}
+	}
+
 }
 
-func checkLink(link string, channel chan string) {
+func checkLink(link string, channel chan int) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down :(")
+		channel <- 1
 		return
 	}
 	fmt.Println(link, "is up :)")
+	channel <- 1
 }
