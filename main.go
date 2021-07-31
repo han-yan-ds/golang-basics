c7a61da55b870689ae31416cfed8073a561c3f3e
han-yan-ds <hanprogramming@gmail.com>
2021-07-31 10:29:55 -0600

    Got it to work

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	shape "golang-basics/shape"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-	sampleTriangle := shape.Triangle{
-		Height: 1.5,
-		Base:   3.0,
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
 	}
-	shape.PrintArea(sampleTriangle)
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down :(")
+		return
+	}
+	fmt.Println(link, "is up :)")
 }
